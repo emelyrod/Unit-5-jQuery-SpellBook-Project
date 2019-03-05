ea55af614c4cb4df427f6c6fd2d4c18994021081
emelyrod <48250829+emelyrod@users.noreply.github.com>
2019-03-05 09:38:53 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,25 @@
+//Spell #1
+
+$("#hide").click(function(){
+    $("#4").hide();
+});
+
+
+
+//Spell #2
+
+$("#ed").click(function(){
+   $("#5").hide();
+});
+
+
+
+
+//Spell #3
+
+$("ee").click(function(){
+ $("#6").hide();
+});
+
+
+
